3031d93d913a9bf9b8295c53d6c4e633730a2c4f
Scholz, Marie <marie.scholz@tecalliance.net>
2022-09-15 11:33:04 +0200

    Created new project

---
+++ Übung 2/Übung 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Übung_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first_number = 1;
+            int second_number = 2;  
+            int sum = first_number + second_number;
+            Console.WriteLine(sum); 
+            Console.ReadLine(); 
+        } 
+    }
+}
