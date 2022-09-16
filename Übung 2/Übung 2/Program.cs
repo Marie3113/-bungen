e4c42bf54f3791506495aff1519b7feb1ee933b4
Scholz, Marie <marie.scholz@tecalliance.net>
2022-09-16 14:24:01 +0200

    Übungsaufgaben

--- Übung 2/Übung 2/Program.cs
+++ Übung 2/Übung 2/Program.cs
@@ -11,27 +11,98 @@ namespace Übung_2
         private static List<string> name;
 
         static void Main(string[] args)
-        {
+        { 
+            //Aufgabe 2
+            List<int> numbers = new List<int> { 5, 3, 7 };
+            //Aufgabe 3
+            string input; 
+            do
+            {
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    Console.WriteLine("Gebe eine Zahl ein");
+                    numbers.Add(Convert.ToInt32(input));
+                }
 
-            Console.WriteLine("Gebe mir eine Zahl");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Bitte gebe noch eine Zahl ein");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            }
+            while (input != "");
 
-            int ergebnis = Add(zahl1, zahl2);
-            Console.WriteLine(ergebnis);
+            //Aufgabe 4
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            
+            //Aufgabe 5
+            numbers.Insert(1, 99);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Aufgabe 6
+
+            List<int> numbers2 = new List<int> { 91, 92, 93 };
+            numbers.AddRange(numbers2);
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Aufgabe 7
+
+            numbers.RemoveAt(0);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Aufgabe 8 
+            numbers.Remove(99);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Aufgabe 9
+            numbers.RemoveAt(numbers.Count -1);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Aufgabe 10
+            numbers.Sort(89);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            numbers.Reverse(89);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            numbers.Remove(<80);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadLine();
 
-            
         }
-        static int Add(int z1, int z2)
-        {
-            int summe = z1 * z2;
-            return summe;
-            Console.WriteLine("Das Erbgenis von " + z1 + z2 + "=" + summe);
-
-        }
+        
     }
+        
     
-} 
+      
+
+}
+
+
+        
