a8843349b5dd8db6d671af0df337f48396de6055
Scholz, Marie <marie.scholz@tecalliance.net>
2022-09-16 10:46:08 +0200

    Listen Einführung

--- Übung 2/Übung 2/Program.cs
+++ Übung 2/Übung 2/Program.cs
@@ -9,44 +9,18 @@ namespace Übung_2
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Bitte gebe eine Zahl ein");
-            double number = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Bitte gebe noch eine Zahl ein");
-            double number2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Was möchtest du mit den Zahlen machen?(+,-,*,/)");
-            string opperator = Console.ReadLine();
-
-            double result = 0;
-
-            switch(opperator)
-            {
-                case "+":
-                    result = number + number2;
-                    break;  
-                    case "-":   
-                    result = number - number2;  
-                    break;
-                case "*":
-                    result = number * number2;
-                    break;
-                case "/":
-                    result = number / number2;
-                    break;
-                    
-
-            }
-
-            Console.WriteLine("Das Erbgenis von " + number + opperator + number2 + "=" + result);
+        { 
+            List<string> names = new List<string>();
+            names.Add("Christian");
+            names.Add("Mona");
 
 
-           
-            
-              
 
-            
+
+
+
+
+
 
             Console.ReadLine();
         }
